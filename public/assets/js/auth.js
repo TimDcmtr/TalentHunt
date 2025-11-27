063c1508164b495cbebca3614282e6a53c662579
leMecha <193751621+TimDcmtr@users.noreply.github.com>
2025-11-27 16:32:42 +0100

    edit auth

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -192,7 +192,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const tab = params.get("tab");
 
   if (tab) {
-    console.log(`Switching to tab: .auth-tab[data-tab="${tab}"]`);
     const button = document.querySelector(`.auth-tab[data-tab="${tab}"]`);
 
     if (button) button.click();
