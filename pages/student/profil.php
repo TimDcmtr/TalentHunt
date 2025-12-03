2e5ee869ba04aedd8c76ad299045b4dfaccfa5dc
leMecha <193751621+TimDcmtr@users.noreply.github.com>
2025-12-03 23:40:57 +0100

    correct error

--- pages/student/profil.php
+++ pages/student/profil.php
@@ -212,7 +212,7 @@
                     <p>PDF • Dernière mise à jour il y a 2 jours</p>
                   </div>
                 </div>
-                <a href="/uploads/cv/<?php $etudiant['cv_filename']; ?>" class="btn-primary">
+                <a href="/uploads/cv/<?= $etudiant['cv_filename']; ?>" class="btn-primary">
                   <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                     <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
                     <polyline points="7 10 12 15 17 10" />
