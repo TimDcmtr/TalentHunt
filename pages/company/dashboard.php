81361e67426e5e8c2a52ea5dda1e393c7e8f8b7b
sh4lk <mateoadnot@gmail.com>
2025-12-04 13:30:34 +0100

    modif

--- pages/company/dashboard.php
+++ pages/company/dashboard.php
@@ -143,7 +143,7 @@
                       </button>
                     </form>
 
-                    <a href="/offre?id=<?php echo $offer['id']; ?>" class="btn-icon" title="Voir l'offre">
+                    <a href="/offer?id=<?php echo $offer['id']; ?>" class="btn-icon" title="Voir l'offre">
                       <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                         <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/>
                         <circle cx="12" cy="12" r="3"/>
