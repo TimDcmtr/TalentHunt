b689f93f4e4eec047517c41bbb9d87ce6a5da884
SwagDeCoq <mquillier@guardiaschool.fr>
2025-12-05 12:27:19 +0100

    retour arrière

--- app/controllers/ApplicationController.php
+++ app/controllers/ApplicationController.php
@@ -4,7 +4,6 @@ require_once ROOT_PATH . 'app/models/Application.php';
 require_once ROOT_PATH . 'app/models/JobOffer.php';
 require_once ROOT_PATH . 'app/models/Company.php';
 require_once ROOT_PATH . 'app/models/User.php';
-MESSAGE = "Données manquantes.";
 class ApplicationController
 {
     private $db;
@@ -33,7 +32,7 @@ class ApplicationController
         if (!isset($data['user_id']) || !isset($data['offre_id'])) {
             http_response_code(400);
             return json_encode([
-                "message" => MESSAGE,
+                "message" => 'Données Manquantes.',
                 "debug" => $data // Pour voir ce qui manque si besoin
             ]);
         }
@@ -203,7 +202,7 @@ class ApplicationController
     {
         if (!isset($data['application_id']) || !isset($data['user_id'])) {
             http_response_code(400);
-            return json_encode(["message" => MESSAGE]);
+            return json_encode(["message" => 'Données Manquantes.']);
         }
 
         if ($this->application->delete($data['application_id'], $data['user_id'])) {
@@ -219,7 +218,7 @@ class ApplicationController
     {
         if (!isset($data['application_id']) || !isset($data['company_id']) || !isset($data['status'])) {
             http_response_code(400);
-            return json_encode(["message" => MESSAGE]);
+            return json_encode(["message" => 'Données Manquantes.']);
         }
 
         if ($this->application->updateStatus($data['application_id'], $data['company_id'], $data['status'])) {
